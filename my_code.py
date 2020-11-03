0bbc053a831e033646c9419651715c4910c2c5bb
jakesheinberg <23jsheinberg@athenian.org>
2020-11-03 12:30:49 -0800

    finshed

--- my_code.py
+++ my_code.py
@@ -2,9 +2,21 @@
 #  
 
 def avg_temp():
+    with open('temps.txt') as file_object:
+        line_list = file_object.readlines()
+        list_length = len(line_list)
+        sum_list = 0
+    for i in range(1, list_length):
+        line_list[i] = line_list[i].rstrip()
+        sum_list = int(line_list[i])+sum_list
+    print(sum_list)
+    average_list = sum_list/(list_length-1)
+    average_list = round(average_list, 2)
+
+    return average_list
 
 
-    return
+
 
 
 if __name__ == '__main__':
